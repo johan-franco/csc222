33d78fbd1823729b070b487263d77f3cdeefc4b4
Johan Franco <johan.francocampos@gmail.com>
2023-09-07 10:10:54 -0400

    added 2 exercises

---
+++ Exercises/ch03/exercise1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+
+void three_lines()
+{
+    new_line(); new_line(); new_line();
+}
+
+int main()
+{
+    cout << "First Line." << endl;
+    newLine();
+    cout << "Second Line." << endl;
+    return 0
+
+void new_line()
+{
+    cout << endl;
+}
---
+++ Exercises/ch03/exercise2.cpp
@@ -0,0 +1,8 @@
+void print_twice(char phil) {
+    cout << phil << phil << endl;
+}
+
+int main() {
+    print_twice('a');
+    return 0;
+}
