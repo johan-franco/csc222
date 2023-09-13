0af57654e1601353ede48d0268db12fc4d60d473
Johan Franco <johan.francocampos@gmail.com>
2023-09-13 08:33:42 -0400

    made single conditional and finished it

---
+++ Exercises/ch05/single_conditional.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age = 30;
+
+    if (age > 16 && age < 65) {
+        cout << "age is within the normal working age." << endl;
+    }
+
+}
