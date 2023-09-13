71cfa6c33c8b9ea59106fcd2f95745ef83b85009
Johan Franco <johan.francocampos@gmail.com>
2023-09-13 08:27:27 -0400

    created and finished test_distance

---
+++ Exercises/ch05/test_distance.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+double distance(double x1, double y1, double x2, double y2) {
+    double dx = x2 - x1;
+    double dy = y2 - y1;
+    double dsquared = dx * dx + dy * dy;
+    double result = sqrt(dsquared);
+    return result;
+}
+int main() {
+    //6, 8 10 triangle, result should be 10
+    double dist = distance(3.0, 2.0, 9.0, 10.0);
+    cout << "dist is: " << dist << endl;
+}
