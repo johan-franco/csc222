432f65e2a1e04aac4878966e87e2bcf786bc7d7d
Johan Franco <johan.francocampos@gmail.com>
2023-09-19 09:50:52 -0400

    renamed some values

--- Exercises/ch07/find_function.cpp
+++ Exercises/ch07/find_function.cpp
@@ -5,7 +5,7 @@ using namespace std;
 void count_letters(string p, char d){
     int count = 0;
     int index = 0;
-    int counter = 0;
+    int limit = 0; /*Starts at zero to make sure that if d is in the first letter it is not skipped*/
     while (index >= 0) {
        index = p.find(d, index+ counter);
        counter = 1;
