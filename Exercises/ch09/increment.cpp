aea1849d0d211587ac3e8c6c34cc05dceb9b6535
Johan Franco <johan.francocampos@gmail.com>
2023-09-28 09:35:47 -0400

    did increment without iteration

--- Exercises/ch09/increment.cpp
+++ Exercises/ch09/increment.cpp
@@ -6,23 +6,38 @@ struct Time {
     double second;
 };
 
-
-void increment(  Time& time,  double secs)
+double convert_to_seconds(const Time& t)
 {
-    time.second += secs;
+    int minutes = t.hour * 60 + t.minute;
+    double seconds = minutes * 60 + t.second;
 
-    while (time.second >= 60.0) {t
-        time.second -= 60.0;
-        time.minute += 1;
-    }
-    while (time.minute >= 60) {
-        time.minute -= 60;
-        time.hour += 1;
-    }
+    return seconds;
+}
+
+Time make_time(double secs)
+{
+    Time time;
+    time.hour = int(secs / 3600.0);
+    secs -= time.hour * 3600.0;
+    time.minute = int(secs / 60.0);
+    secs -= time.minute * 60;
+    time.second = secs;
+
+    return time;
+}
+
+Time increment( Time& time, const double sec)
+{
+    return make_time(convert_to_seconds(time) + sec);
 }
 
 int main() {
+    double secs = 103.4;
+    cout << time.hour<< ":" << time.minute << ":" << time.second;
+    
     Time time = {11, 59, 3.14159};
-    increment(time, );
+    time = increment(time, secs);
+    cout << time.hour<< ":" << time.minute << ":" << time.second;
+    
     return 0;
 }
