a4435367f7a243fc26c8148021b5ce22479d9848
Johan Franco <johan.francocampos@gmail.com>
2023-10-16 19:46:07 -0400

    stuck on tree problem worked on

--- projects/kitten.cpp
+++ projects/kitten.cpp
@@ -3,29 +3,59 @@
 #include <cstring>
 #include <string>
 #include <list>
+#include <iterator>
 #include <vector>
+#include <sstream>
+
 using namespace std;
 
+template <size_t N>
+void splitString(string (&arr)[N], string str)
+{
+    int n = 0;
+    istringstream iss(str);
+    for (auto it = istream_iterator<string>(iss); it != istream_iterator<string>() && n < N; ++it, ++n)
+        arr[n] = *it;
+}
 
-void tree(vector<string> branches) {
-    string kitten_branch, input, start, brancha;
 
-    /*while(stoi(input) != -1 ) {
+
+void tree() {
+    string kitten_branch, start, brancha, input, branch_num;
+    int branch = 0, branches = 0;
+    while(input != "-1" ) {
         getline(cin, input);
-        branches.push_back(input);
-        cout << input;
-    } */
-    start= getline(cin, input);
-    cout << start<< endl;
-    brancha = getline(cin, input);
-    cout << brancha;
+        string tree[branch][branches]; 
+        stringstream in(input);
+        while(getline(in, branch_num, ' ')) {
+            string temptree[branch][branches];
+            tree[branch][branches] = branch_num;
+            branches+=1;
+            temptree = tree;
+        }
+        branch+=1;
 
+        
+        /*int i = 0;
+        int space = 0;
+        while(i <= input[i] ){
+            if( input[i] ==' '){
+                space++;
+            }
+        i++;
+        }
+        string arr[i];
+        splitString(arr, input);
+        branches.push_back(arr);
+        cout << input;
+        */
+    } 
 
 }
 
 int main(){
-    vector<string> branches;
-    tree(branches);
+    
+    tree();
     
     return 0;
 }
