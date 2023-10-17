c1c963a49d79da647d4c3572776ac94cd490fd98
Johan Franco <johan.francocampos@gmail.com>
2023-10-17 18:58:40 -0400

    function call error

--- projects/kitten.cpp
+++ projects/kitten.cpp
@@ -3,40 +3,10 @@
 #include <sstream>
 using namespace std;
 
-void expandArray(string*** arr, int* rows, int* columns, int newRows, int newColumns) {
-    string** newArr = new string*[newRows];
-    for (int i = 0; i < newRows; i++) {
-        newArr[i] = new string[newColumns];
-    }
-
-    // Copy values from the old array to the new array
-    for (int i = 0; i < *rows; i++) {
-        for (int j = 0; j < *columns; j++) {
-            newArr[i][j] = (*arr)[i][j];
-        }
-    }
-
-    // Deallocate memory for the old array
-    for (int i = 0; i < *rows; i++) {
-        delete[] (*arr)[i];
-    }
-    delete[] *arr;
-
-    // Update the pointer to point to the new array
-    *arr = newArr;
-
-    *rows = newRows;
-    *columns = newColumns;
-}
-
-int main() {
-    string** tree = new string*[100]; 
-    for (int i = 0; i < 100; i++) {
-        tree[i] = new string[100]; 
-    }
-    int rows = 0, columns = 0;
+void insert_val(string arr[], int* rows, int* columns) {
+    string temp[100][100];
     string input;
-
+    int max_columns = 0;
     while (input != "-1") {
         getline(cin, input);
         stringstream in(input);
@@ -44,21 +14,34 @@ int main() {
         string branch_num;
         int i = 0;
         while (getline(in, branch_num, ' ')) {
-            if (columns <= i) {
-                expandArray(&tree, &rows, &columns, rows, i + 1);
-                columns = i + 1;
-            }
-            tree[rows][i] = branch_num;
-            i++;
+            *arr[*rows][*columns] = branch_num;
+            columns++;
+            max_columns = (*columns > max_columns) ? *columns:  max_columns;
         }
         rows++;
     }
-
-    // Deallocate memory for the tree array when done
-    for (int i = 0; i < rows; i++) {
-        delete[] tree[i];
+    cout << temp <<endl;
+    for (int i = 0; i < *rows; i++) {
+        for (int j = 0; j < *columns; j++) {
+            cout << temp[i][j] <<endl;
+        }
     }
-    delete[] tree;
+
+
+  
+    
+    *columns = max_columns;
+
+
+}
+
+int main() {
+    string tree[100][100]; 
+    int rows = 0, columns = 0;
+    
+    insert_val(tree, rows,columns);
+    string input;
+
 
     return 0;
 }
