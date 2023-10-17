f1e89af66c92ff4788e5b2fc820d3c5a61cd6710
Johan Franco-Campos <johan.francocampos@gmail.com>
2023-10-17 19:04:38 -0400

    changes to try to fix call error

--- projects/kitten.cpp
+++ projects/kitten.cpp
@@ -3,8 +3,7 @@
 #include <sstream>
 using namespace std;
 
-void insert_val(string arr[], int* rows, int* columns) {
-    string temp[100][100];
+void insert_val(string arr[][100], int& rows, int& columns) {
     string input;
     int max_columns = 0;
     while (input != "-1") {
@@ -14,34 +13,27 @@ void insert_val(string arr[], int* rows, int* columns) {
         string branch_num;
         int i = 0;
         while (getline(in, branch_num, ' ')) {
-            *arr[*rows][*columns] = branch_num;
-            columns++;
-            max_columns = (*columns > max_columns) ? *columns:  max_columns;
+            arr[rows][i] = branch_num;
+            i++;
         }
+        max_columns = (i > max_columns) ? i : max_columns;
         rows++;
     }
-    cout << temp <<endl;
-    for (int i = 0; i < *rows; i++) {
-        for (int j = 0; j < *columns; j++) {
-            cout << temp[i][j] <<endl;
-        }
-    }
 
-
-  
-    
-    *columns = max_columns;
-
-
+    columns = max_columns;
 }
 
 int main() {
-    string tree[100][100]; 
+    string tree[100][100];
     int rows = 0, columns = 0;
     
-    insert_val(tree, rows,columns);
-    string input;
+    insert_val(tree, rows, columns);
 
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns; j++) {
+            cout << tree[i][j] << endl;
+        }
+    }
 
     return 0;
 }
