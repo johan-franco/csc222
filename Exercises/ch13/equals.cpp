0294f4d355342d929895c4f960229aeb22cd3c6d
Johan Franco <johan.francocampos@gmail.com>
2023-10-24 08:59:50 -0400

    finished equals.cpp

--- Exercises/ch13/equals.cpp
+++ Exercises/ch13/equals.cpp
@@ -16,3 +16,17 @@ Card::Card() {
 Card::Card(int s, int r) {
     suit = s; rank = r;
 }
+
+
+bool equals(const Card& c1, const Card& c2) 
+{
+    return (c1.rank == c2.rank && c1.suit == c2.suit);
+}
+int main() {
+    Card card1(1, 11);
+    Card card2(1, 11);
+    Card card3(1, 10);
+
+    cout << "Comparing 1 & 2: " << equals(card1,card2)<< endl;
+    cout << "Comparing 2 & 3: " << equals(card2,card3)<< endl;
+}
