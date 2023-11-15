7b0e6fcf510ab2eb15dde435a0a783012eebb805
Johan Franco <johan.francocampos@gmail.com>
2023-11-15 09:24:32 -0500

    started on 14 exercise 2

---
+++ Exercises/ch14/random.cpp
@@ -0,0 +1,4 @@
+#include "random.h"
+int random_between(int l, int h) {
+    int max = 
+}
---
+++ Exercises/ch14/random.h
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cstdlib>
+
+using namespace std;
+
+int random_between(int l, int h);
