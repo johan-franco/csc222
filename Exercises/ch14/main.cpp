d8a8058878b2da833f233ff812e20a7272ccbb30
Johan Franco-Campos <johan.francocampos@gmail.com>
2023-11-21 23:03:11 -0500

    added main using function

--- Exercises/ch14/main.cpp
+++ Exercises/ch14/main.cpp
@@ -9,6 +9,9 @@ int main() {
     mydeck.swap(20,30);
     cout << "After Swap:"<< endl << mydeck.cards[20].to_string() << " " << mydeck.cards[30].to_string() << endl;
 
+    int lowestIndex = mydeck.find_lowest(10, 42);
+    cout << "Index of the lowest card between 0 and 42: " << lowestIndex << endl << mydeck.cards[lowestIndex].tostring();
+
     
 
     return 0;
