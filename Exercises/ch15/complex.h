29ee13fb2e486d53b88c0c05802d7439e6d79057
Johan Franco <johan.francocampos@gmail.com>
2023-11-30 08:59:15 -0500

    added accesor functions and cal polar

--- Exercises/ch15/complex.h
+++ Exercises/ch15/complex.h
@@ -1,3 +1,9 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#pragma once
+
+using namespace std;
 class Complex {
     double real, imag;
     double mag, theta;
@@ -6,5 +12,24 @@ class Complex {
     public:
     Complex() {real = 0; imag = 0; polar = false;}
     Complex(double r,double i) {real = r; imag = i; polar = false;}
-    
+
+    void Complex::calculate_polar(){
+        mag = sqrt(real * real + imag * imag);
+        theta = atan(imag / real);
+        polar = true;
+    }
+    double Complex::get_real() {
+        return real;
+    }
+    double Complex::get_imag() {
+        return imag;
+    }
+    double Complex::get_mag() {
+        if (polar = false) calculate_polar();
+        return mag;
+    }
+    double Complex::get_theta() {
+        if (polar = false) calculate_polar();
+        return theta;
+    }
 };
