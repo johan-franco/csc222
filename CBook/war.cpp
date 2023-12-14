d0076483768cec072c7a90e66e3952da2762a838
Johan Franco <johan.francocampos@gmail.com>
2023-12-14 10:29:45 -0500

    zsh seg fault

--- CBook/war.cpp
+++ CBook/war.cpp
@@ -12,13 +12,28 @@ int main(){
     d1.shuffle();
 
     copy(d1.cards.begin(), d1.cards.begin()+26,p1.cards.begin());
-    copy(d1.cards.begin()+26, d1.cards.begin()+52,p2.cards.begin());
+    copy(d1.cards.begin()+26, d1.cards.end(),p2.cards.begin());
 
-    Card c1 = p1.popCard();
-    Card c2 = p2.popCard();
+    while(p1.isEmpty() == false && p2.isEmpty() == false) {
+        if (p1.isEmpty() || p2.isEmpty()) {
+            break;
+        }
+        Card c1 = p1.popCard();
+        Card c2 = p2.popCard();
 
-    int diff = c1.rank - c2.rank;
-    cout << diff;
+        
+        int diff = c1.rank - c2.rank;
+        cout << diff;
+        if (diff > 0) {
+            p1.addCard(c1);
+            p1.addCard(c2);
+        } else if (diff < 0) {
+            p2.addCard(c1);
+            p2.addCard(c2);
+        } else {
+            continue;
+        }
+    } 
 
     return 0;
 }
